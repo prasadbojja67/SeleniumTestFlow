f70d39bdb7fea659fa7b253039a62c6dca20998c
prasadbojja67 <125078589+prasadbojja67@users.noreply.github.com>
2024-10-14 19:31:12 +0530

    Add files via upload

---
+++ javatest.java
@@ -0,0 +1,30 @@
+class Javatest
+	{
+public static void main(String args[])
+	{
+	String project_name="commercial cards";
+	System.out.println("the project name is   "+" "+project_name);
+	int card_number=102101010;
+	System.out.println("the card number is   "+" "+card_number);
+	int transaction_id;
+    transaction_id=24873;
+	System.out.println("the transaction id is   "+" "+transaction_id);
+   transaction_id=5000;
+	System.out.println("the transaction id is   "+" "+transaction_id);
+	float price=10.20f;
+	String company="WIPRO";
+    boolean job=true;
+	char gender='M';
+	String name="Prasad";
+	System.out.println("Hi"+"   "+name);
+	String last_Name="Bojja";
+System.out.println(last_Name+"  "+name);
+	int a=10,b=20;
+	int c=a+b;
+	System.out.println("sum is "+c);
+	int x,y,z;
+	x=y=z=30;
+	System.out.println(x+y+z);
+	}
+
+}
