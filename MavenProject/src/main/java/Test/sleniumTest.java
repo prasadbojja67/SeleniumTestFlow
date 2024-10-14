79c6979db530ac7c3a9666aaa9aa44b24966e2bc
bojjaprasad505 <bojjaprasad505@gmail.com>
2024-10-14 19:25:32 +0530

    My first commit of selenium project

---
+++ MavenProject/src/main/java/Test/sleniumTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+/**
+ * 
+ */
+public class sleniumTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+WebDriver driver=new ChromeDriver();
+driver.get("https://google.com/");
+System.out.println("Test Pass");
+
+	}
+
+}
